6caa49b65fb96d81d5731f81b9736915384188a8
diasSarekpera <154084819+diasSarekpera@users.noreply.github.com>
2026-01-05 06:10:28 -0800

    Add files via upload

--- js/home/wa-message.js
+++ js/home/wa-message.js
@@ -1,23 +1,22 @@
-document.querySelectorAll('.product-card').forEach(card => {
-    card.addEventListener('click', () => {
+document.querySelectorAll('.product-whatsapp').forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault(); // empêche le lien #
+
+        const card = button.closest('.product-card');
+
         const name = card.dataset.name;
         const image = card.dataset.image;
+        const price = card.dataset.price;
 
-        const message = `Bonjour Arie's accessoire, je suis intéressé le produit "${name}".\nImage: ${image}`;
-        const phone = "22954959597";
+        const message = `Bonjour Arie's accessoire
+Je suis intéressé par le produit :
+${name}
+Prix : ${price}
+Image : ${image}`;
 
+        const phone = "22940260611";
         const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
         window.open(url, "_blank");
     });
 });
-
-
-
-
-
-
-
-
-
-
-
